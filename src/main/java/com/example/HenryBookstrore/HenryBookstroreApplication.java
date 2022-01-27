172019298cda01e1e479f71785900a5679ab1121
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-01-27 13:19:47 +0200

    first commit

---
+++ src/main/java/com/example/HenryBookstrore/HenryBookstroreApplication.java
@@ -0,0 +1,13 @@
+package com.example.HenryBookstrore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenryBookstroreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenryBookstroreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HenryBookstrore/HenryBookstroreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HenryBookstrore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenryBookstroreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
